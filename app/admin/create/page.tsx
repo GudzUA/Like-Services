8fd889740b81381c88a057a7d57adfc0b2860051
Yaroslav <gudz80@gmail.com>
2025-07-27 14:35:02 -0500

    fix: rename create-all API to route.ts for Vercel build

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -43,10 +43,12 @@ if (photoFile) {
   const formData = new FormData();
   formData.append("image", photoFile);
 
-  const res = await fetch(`https://api.imgbb.com/1/upload?key=YOUR_API_KEY`, {
-    method: "POST",
-    body: formData,
-  });
+  const res = await fetch(`https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`, {
+  method: "POST",
+  body: formData,
+});
+
+
 
   const data = await res.json();
   if (data.success) {
