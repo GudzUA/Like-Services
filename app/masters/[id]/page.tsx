74fac19653ab8e00ece850232c88fbeab4c4f756
Yaroslav <gudz80@gmail.com>
2025-07-27 17:53:48 -0500

    Fix master page crash on Vercel

--- app/masters/[id]/page.tsx
+++ app/masters/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { prisma } from "@/lib/prisma";
 import MasterBookingClient from "@/components/MasterBookingClient";
 import { notFound } from "next/navigation";
