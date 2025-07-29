0dcb8a0969fd6e2a1ec8970dcdfaaa16d07ba103
Yaroslav <gudz80@gmail.com>
2025-07-29 00:46:51 -0500

    Готово для деплою

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
-  })
+    log: ["error"], // 🔇 показує тільки помилки, без query
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
